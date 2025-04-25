ff157a4863e8674b9671c071aa63f5141833b8f7
Venkat Dara <venkatdara133@gmail.com>
2025-04-24 22:51:39 -0500

    fixed models.py

--- app/models.py
+++ app/models.py
@@ -6,11 +6,12 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model, UserMixin):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-    expenses = db.relationship('Expense', backref='owner', lazy=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
